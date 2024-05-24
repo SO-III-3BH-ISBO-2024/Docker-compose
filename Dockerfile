325d36e2f20eb090b9e17e9c2e1964edf2affb91
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2024-05-23 21:10:04 -0300

    Primer commit. Tenemos un proyecto y un docker file

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM centos:7 
+
+RUN yum install -y php php-mysql httpd 
+
+COPY *.php /var/www/html/
+
+CMD httpd -D FOREGROUND
---
+++ configuracion.php
@@ -0,0 +1,5 @@
+<?php 
+    $ipBaseDeDatos = getenv("IP_BASE_DE_DATOS");
+    $usuarioBaseDeDatos = getenv("USUARIO_BASE_DE_DATOS");
+    $contrasenaBaseDeDatos = getenv("CONTRASENA_BASE_DE_DATOS");
+    $nombreBaseDeDatos = getenv("NOMBRE_BASE_DE_DATOS");
